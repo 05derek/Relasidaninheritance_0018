51f5cd6bec4f6e318f9c0f527c8d33a10aa6ff3a
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-26 18:23:20 +0700

    proses algoritma function nya

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -1,16 +1,9 @@
 
 
-class pelajar : public orang {
-public :
-    string sekolah;
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
 
-    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
-        cout << "Pelajar dibuat\n" << endl;
-    }
-    ~pelajar() {
-        cout << "Pelajar dihapus\n" << endl;
-    }
-    string perkenalan() {
-        return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n"
-    }
-};
+    return 0;
+}
